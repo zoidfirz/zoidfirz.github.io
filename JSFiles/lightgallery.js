6f254e871d3d42ef42eaa72830f9fcf521ea7272
jason sutherland <zoidfirz@gmail.com>
2019-12-20 09:23:24 -0500

    second part of commit

---
+++ JSFiles/lightgallery.js
@@ -0,0 +1,16 @@
+//
+// $(document).ready(function(){
+//     $(".fancybox").fancybox({
+//         openEffect: "none",
+//         closeEffect: "none"
+//     });
+//
+//     $(".zoom").hover(function(){
+//
+//         $(this).addClass('transition');
+//     }, function(){
+//
+//         $(this).removeClass('transition');
+//     });
+// });
+//
